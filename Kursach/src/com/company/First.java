e2cefc57f123c2cf013260b66761c4f695042916
verrrmoot <55789200+verrrmoot@users.noreply.github.com>
2021-05-25 09:26:39 +0300

    Add files via upload

--- Kursach/src/com/company/First.java
+++ Kursach/src/com/company/First.java
@@ -1,92 +1,118 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class First {
     public static void main(String[] args) {
-        int[] arr1 = {2, 1, 2};
-        maxPerimeter(arr1, 3);
-        int[] arr2 = {3, 6, 2, 3};
-        maxPerimeter(arr2, 4);
-        int[] arr3 = {33, 6, 20, 1, 8, 12, 5, 55, 4, 9};
-        maxPerimeter(arr3, 10);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Задание 1:");
+        System.out.println("Введите первую строку");
+        String s1 = in.nextLine();
+        System.out.println("Введите вторую строку");
+        String s2 = in.nextLine();
 
-        int[] nums = {2, 10, 4};
-        System.out.println(largestNumber(nums));
-
-        int[][] diagonal = new int[3][4];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                diagonal[i][j] = (int) (Math.random() * 6);
+        System.out.println("Результат: " + checkIfCanBreak(s1,s2));
+        System.out.println("Задание 2:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String str1 =scanner.nextLine();
+        System.out.println("Введенная строка: " + str1);
+        System.out.println("Длина самой большой строки полиндрома: " + longPalSubstr(str1));
+        System.out.println("Задание 3:");
+        System.out.println("Введите строку:");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println( distinctEchoSubstrings(s));
+    }
+    static void printSubStr(String str1, int l, int h) {
+        System.out.println(str1.substring(l, h + 1));
+    }
+    static int longPalSubstr(String str1) {
+        int n = str1.length();
+        boolean table[][] = new boolean[n][n];
+        int mLength = 1;
+        for (int i = 0; i < n; ++i)
+            table[i][i] = true;
+        int strt = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (str1.charAt(i) == str1.charAt(i + 1)) {
+                table[i][i + 1] = true;
+                strt = i;
+                mLength = 2;
             }
         }
-        System.out.println(Arrays.deepToString(diagonalSort(diagonal)));
-    }
-
-    // Задание №1. Максимальный периметр треугольника
-    static void maxPerimeter(int[] arr, int n) {
-        int maxP = 0; // Создаём переменную, который будет хранить максимальное значение периметра
-        for (int i = 0; i < n - 2; i++) { // Берём различные значения из массива
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    int a = arr[i]; // Присваиваем их значения сторонам
-                    int b = arr[j];
-                    int c = arr[k];
-                    if (a < b + c && b < c + a && c < a + b) { // Если можно образовать треугольник
-                        maxP = Math.max(maxP, a + b + c); // То переменной присваивается максимальное значение
+        for (int k = 3; k <= n; ++k) {
+            for (int i = 0; i < n - k + 1; ++i) {
+                int j = i + k - 1;
+                if (table[i + 1][j - 1] && str1.charAt(i) == str1.charAt(j)) {
+                    table[i][j] = true;
+                    if (k > mLength) {
+                        strt = i;
+                        mLength = k;
                     }
                 }
             }
         }
-        if (maxP > 0) // Если периметр поменялся
-            System.out.println("Максимальный периметр: " + maxP); //Выводим значение
-        else // Иначе выводим ошибку
-            System.out.println("Образование треугольника из таких значений невозможно");
+        System.out.print("Наибольшая строка полиндром в ввведенной строке: ");
+        printSubStr(str1, strt, strt + mLength - 1);
+        return mLength;
+    }
+    public static boolean checkIfCanBreak(String s1, String s2) {
+        int []arr1=new int[26];
+        int []arr2=new int[26];
+
+        for(int i=0;i<s1.length();i++){
+            arr1[s1.charAt(i)-'a']++;
+        }
+
+        for(int i=0;i<s2.length();i++){
+            arr2[s2.charAt(i)-'a']++;
+        }
+
+        int count1=0;
+        int count2=0;
+        int greater=0;
+        int smaller=0;
+
+        for(int i=0;i<26;i++){
+            count1+=arr1[i];
+            count2+=arr2[i];
+
+            if(count2>count1){
+                smaller++;
+            }else if(count1>count2){
+                greater++;
+            }
+            if(smaller>0 && greater>0)
+                return false;
+        }
+        return true;
+    }
+    public static int distinctEchoSubstrings(String text) {
+        if (text == null || text.length() == 0) return 0;
+        Set<String> stringSet = new HashSet<>();
+
+        for (int right = 1; right <= text.length(); ++right)
+        {
+            for (int left = 0; left < right; ++left)
+            {
+                if (right - left <= 1) continue;
+                String subStr = text.substring(left, right);
+                if (isEchoString(subStr)) {
+                    stringSet.add(subStr);
+                }
+            }
+        }
+
+        return stringSet.size();
     }
 
-    // Задача №2. Максимальное число
-    private static class LargerNumberComparator implements Comparator<String> {
-        @Override
-        public int compare(String a, String b) {
-            String order1 = a + b;
-            String order2 = b + a;
-            return order2.compareTo(order1);
-        }
-    }
+    private static boolean isEchoString(String subStr)
+    {
+        if (subStr.length() % 2 != 0) return false;
 
-    public static String largestNumber(int[] nums) {
-        String[] asStrs = new String[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            asStrs[i] = String.valueOf(nums[i]);
-        }
-        Arrays.sort(asStrs, new LargerNumberComparator());
-        if (asStrs[0].equals("0")) {
-            return "0";
-        }
-        StringBuilder largestNumberStr = new StringBuilder("");
-        for (String numAsStr : asStrs) {
-            largestNumberStr.append(numAsStr);
-        }
-        return largestNumberStr.toString();
-    }
-
-
-    // Задача №3. Сортировка диагоналей в матрице
-    public static int[][] diagonalSort(int[][] M) {
-        int y = M.length, x = M[0].length - 1;
-        int[] diag = new int[y];
-        for (int i = 2 - y; i < x; i++) {
-            int k = 0;
-            for (int j = 0; j < y; j++)
-                if (i + j >= 0 && i + j <= x)
-                    diag[k++] = M[j][i + j];
-            Arrays.sort(diag, 0, k);
-            k = 0;
-            for (int j = 0; j < y; j++)
-                if (i + j >= 0 && i + j <= x)
-                    M[j][i + j] = diag[k++];
-        }
-        return M;
+        if (subStr.substring(0, subStr.length()/2).equals(subStr.substring(subStr.length()/2, subStr.length())))
+            return true;
+        return false;
     }
 }
